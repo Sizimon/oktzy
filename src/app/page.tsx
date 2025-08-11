00fd9c9805e837173e4d594411bba1e7bc7cdc9a
Shhmon <Siizmonsus@gmail.com>
2025-08-11 19:11:01 +0200

    FEAT: Added time formatting to convert seconds into HH:MM:SS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,20 @@ export default function Home() {
     const [clipUrl, setClipUrl] = useState<string>('');
     const [timestampModalOpen, setTimestampModalOpen] = useState<boolean>(false);
     const [currentTime, setCurrentTime] = useState<number>(0);
-    console.log('Clip URL:', clipUrl);
-    console.log('Current Time:', currentTime);
+    const [timestampTitle, setTimestampTitle] = useState<string>('');
+    const [timestampNote, setTimestampNote] = useState<string>('');
+    // console.log('Clip URL:', clipUrl);
+    // console.log('Current Time:', currentTime);
+
+    function formatCurrentTime(time: number): string {
+      const hours = Math.floor(time / 3600);
+      const minutes = Math.floor((time % 3600) / 60);
+      const seconds = Math.floor(time % 60);
+      if (hours > 0) {
+        return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+      }
+      return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+    }
 
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setClipUrl(event.target.value);
@@ -33,8 +45,7 @@ export default function Home() {
   return (
     <div className="font-sans flex flex-col items-center justify-items-center h-lvh p-8">
       <div>
-        <label htmlFor="clip-url" className="text-lg font-medium">Clip URL:</label>
-        <input id="clip-url" value={clipUrl} onChange={handleInputChange} type="text" className="mt-2 p-2 border border-gray-300 rounded" />
+        <input id="clip-url" value={clipUrl} placeholder='Enter clip URL...' onChange={handleInputChange} type="text" className="mt-2 p-2 border border-gray-300 rounded" />
       </div>
       <div className="mt-8 w-full flex justify-center">
         <VideoDisplay clipUrl={clipUrl} modalOpen={timestampModalOpen} setCurrentTime={setCurrentTime} />
@@ -44,10 +55,11 @@ export default function Home() {
       </div>
       {timestampModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/30">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <h2 className="text-xl font-semibold mb-4">Timestamp Added</h2>
-            <p className="mb-4">Timestamp: {new Date().toLocaleTimeString()}</p>
-            <button className="p-2 bg-red-500 text-white rounded" onClick={() => setTimestampModalOpen(false)}>Close</button>
+          <div className="flex flex-col justify-center items-center bg-white p-6 rounded shadow-lg w-2/6">
+            <p>{formatCurrentTime(currentTime)}</p>
+            <input type="text" placeholder='Timestamp title...' value={timestampTitle} maxLength={24} onChange={(e) => setTimestampTitle(e.target.value)}  className="my-4 p-2 border border-gray-300 rounded" />
+            <textarea placeholder='Timestamp note...' value={timestampNote} onChange={(e) => setTimestampNote(e.target.value)} className="mb-4 p-2 border border-gray-300 rounded w-full" />
+            <button className="p-2 bg-red-500 text-white rounded cursor-pointer" onClick={() => setTimestampModalOpen(false)}>Close</button>
           </div>
         </div>
       )}
