7fd2753692a5fab1f1fc9716626e355ceb0264fe
Szymon <siizmonsus@gmail.com>
2025-09-12 23:22:04 +0200

    FEAT: Sorted timestamps before rendering to ensure they are sorted based on time.

--- src/features/clips/components/ui/ClipNoteDisplay.tsx
+++ src/features/clips/components/ui/ClipNoteDisplay.tsx
@@ -5,6 +5,8 @@ import { Accordion, AccordionItem, AccordionContent, AccordionTrigger } from '@/
 
 
 export const ClipNoteDisplay: React.FC<ClipSidebarProps> = ({ timestamps, handleToTimestamp }) => {
+    const sortedTimestamps = [...timestamps].sort((a, b) => a.time - b.time);
+
     return (
         <div className='p-4 rounded-2xl w-full h-full flex flex-col'>
             {timestamps.length === 0 ? (
@@ -12,25 +14,25 @@ export const ClipNoteDisplay: React.FC<ClipSidebarProps> = ({ timestamps, handle
                     <p className="text-center">No timestamps available, click "Add Timestamp" to create one.</p>
                 </div>
             ) : (
-                    <Accordion type="single" collapsible className="w-full flex-1 overflow-y-auto" defaultValue='ERROR'>
-                        {timestamps.map((timestamp, index) => (
+                    <Accordion type="single" collapsible className="w-full flex-1 overflow-y-auto no-scrollbar">
+                        {sortedTimestamps.map((st, index) => (
                             <AccordionItem key={index} value={`item-${index}`}>
                                 <AccordionTrigger>
                                     <div className="flex justify-start items-center w-full">
-                                        <span className="font-medium uppercase">{timestamp.title}</span>
+                                        <span className="font-medium uppercase">{st.title}</span>
                                     </div>
                                     <span 
                                         onClick={(e) => {
                                             e.stopPropagation();
-                                            handleToTimestamp(timestamp.time);
+                                            handleToTimestamp(st.time);
                                         }}
                                         className="text-sm text-gray-500 ml-2 hover:text-violet-700 cursor-pointer">
-                                        {timestamp.timeStringConverted}
+                                        {st.timeStringConverted}
                                     </span>
                                 </AccordionTrigger>
                                 <AccordionContent>
                                     <div className="justify-center items-center w-full bg-foreground p-4 rounded">
-                                        <p className="whitespace-pre-wrap text-start">{timestamp.note}</p>
+                                        <p className="whitespace-pre-wrap text-start">{st.note}</p>
                                     </div>
                                 </AccordionContent>
                             </AccordionItem>
