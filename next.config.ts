6fb804456e55a9185388e5c05a1c841ccf079b20
Sizimon <siizmonsus@gmail.com>
2025-09-15 20:09:58 +0200

    FIX: Ignore eslint during builds and change next config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  basePath: '/noto',
+  assetPrefix: '/noto',
+  trailingSlash: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
