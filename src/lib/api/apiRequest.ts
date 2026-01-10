91e65e1ab8233da1f175da52e6f3c78dc80b0531
Sizimon <siizmonsus@gmail.com>
2026-01-10 01:49:34 +0100

    FEAT: Altered api base url, also altered project name in package json

--- src/lib/api/apiRequest.ts
+++ src/lib/api/apiRequest.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5050/api';
 
 const apiRequest = async (endpoint: string, options: RequestInit) => {
     const config: RequestInit = {
